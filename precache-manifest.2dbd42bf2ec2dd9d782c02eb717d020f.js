3c16718d5cba602d47f016c166f1458a963d5947
Manas Muda <manasgupta435@gmail.com>
2020-10-19 13:55:37 +0530

    build init

---
+++ precache-manifest.2dbd42bf2ec2dd9d782c02eb717d020f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a60880a215a65cec41941e88c8ad8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cccd1e2725c9aa63052",
+    "url": "/static/css/2.fad0a8c6.chunk.css"
+  },
+  {
+    "revision": "a68e51fd5aef44166880",
+    "url": "/static/css/main.fca4dafe.chunk.css"
+  },
+  {
+    "revision": "2cccd1e2725c9aa63052",
+    "url": "/static/js/2.c18340a0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c18340a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68e51fd5aef44166880",
+    "url": "/static/js/main.2b9b343f.chunk.js"
+  },
+  {
+    "revision": "f2bf9baf648c9812b682",
+    "url": "/static/js/runtime-main.b1f2e438.js"
+  },
+  {
+    "revision": "ed9223b46a7767df0f842300f83ce157",
+    "url": "/static/media/Sansation_Bold_Italic.ed9223b4.ttf"
+  },
+  {
+    "revision": "99f7456dfd298c4545c7687ce8d5539d",
+    "url": "/static/media/Sansation_Light.99f7456d.ttf"
+  },
+  {
+    "revision": "2f4dbbdb2a37e01fc3aa8d8172c66d2e",
+    "url": "/static/media/games_mock.2f4dbbdb.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bff481a7b2e399fe430e23bf63052f08",
+    "url": "/static/media/play_store_logo.bff481a7.png"
+  },
+  {
+    "revision": "8af6e8fb492f9762a462596ccb093101",
+    "url": "/static/media/potrait_logo.8af6e8fb.png"
+  },
+  {
+    "revision": "e08894c265ac251b60ea76c7093fd7f5",
+    "url": "/static/media/tactical_combat_icon.e08894c2.png"
+  }
+]);
